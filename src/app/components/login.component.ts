ca8836ecf7621d453ffa194df73c4dadfb372aae
Rushil Perera <pererarushil1940@gmail.com>
2016-11-28 21:00:47 -0500

    Adding login component

---
+++ src/app/components/login.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from "@angular/core";
+import {User} from "../models/user.model";
+import {Router} from "@angular/router";
+import {UserService} from "../services/fake-user.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: 'templates/login.component.html',
+  styleUrls: ['stylesheets/login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user = new User('', '');
+  active = true;
+
+  constructor(private _userService: UserService, private _router: Router) {
+  }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.active = false;
+    this._userService.login(this.user).subscribe((res) => {
+      if (res.status == 200) {
+        this._router.navigate(['']);
+      }
+    });
+    setTimeout(() => this.active = true, 0);
+  }
+
+}
