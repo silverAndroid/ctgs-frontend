7cf414a89987617a522aaaf60924953ca3838ca6
Rushil Perera <pererarushil1940@gmail.com>
2016-11-28 20:56:38 -0500

    Moved tests to own section

--- src/app/tests/app.component.spec.ts
+++ src/app/tests/app.component.spec.ts
@@ -18,16 +18,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'Travel Grant Application System'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('Travel Grant Application System');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in toolbar tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('md-toolbar').querySelector('span').textContent).toContain('Travel Grant Application System');
   }));
 });
