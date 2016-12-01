7ee88a05d056542e9144005327defdc12b9325c9
Rushil Perera <pererarushil1940@gmail.com>
2016-12-01 16:26:01 -0500

    Added encapsulation to fields

--- src/app/models/student-application.model.ts
+++ src/app/models/student-application.model.ts
@@ -2,10 +2,42 @@
  * Created by silver_android on 30/11/16.
  */
 export class StudentApplication {
-  constructor(public registrationCost: number, public transportationCost: number, public accomodationCost: number, public mealCost: number) {
-    this.registrationCost = Number(registrationCost.toFixed(2));
-    this.transportationCost = Number(transportationCost.toFixed(2));
-    this.accomodationCost = Number(accomodationCost.toFixed(2));
-    this.mealCost = Number(mealCost.toFixed(2));
+  constructor(private _registrationCost: number, private _transportationCost: number, private _accommodationCost: number, private _mealCost: number) {
+    this.registrationCost = Number(this.registrationCost.toFixed(2));
+    this.transportationCost = Number(this.transportationCost.toFixed(2));
+    this.accommodationCost = Number(this.accommodationCost.toFixed(2));
+    this.mealCost = Number(this.mealCost.toFixed(2));
+  }
+
+  get registrationCost(): number {
+    return this._registrationCost;
+  }
+
+  set registrationCost(value: number) {
+    this._registrationCost = value;
+  }
+
+  get transportationCost(): number {
+    return this._transportationCost;
+  }
+
+  set transportationCost(value: number) {
+    this._transportationCost = value;
+  }
+
+  get accommodationCost(): number {
+    return this._accommodationCost;
+  }
+
+  set accommodationCost(value: number) {
+    this._accommodationCost = value;
+  }
+
+  get mealCost(): number {
+    return this._mealCost;
+  }
+
+  set mealCost(value: number) {
+    this._mealCost = value;
   }
 }
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -3,6 +3,45 @@
  */
 export class User {
 
-  constructor(public username: string, public password: string, public surname?: string, public givenNames?: string, public email?: string) {
+  constructor(private _username: string, private _password: string, private _role: string, private _name?: string, private _email?: string) {}
+
+  get username(): string {
+    return this._username;
+  }
+
+  set username(value: string) {
+    this._username = value;
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
+  set password(value: string) {
+    this._password = value;
+  }
+
+  get role(): string {
+    return this._role;
+  }
+
+  set role(value: string) {
+    this._role = value;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  set email(value: string) {
+    this._email = value;
   }
 }
