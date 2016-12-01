b1ca4fd20a03e0c30cc71f690d9e1c43a48492f2
Rushil Perera <pererarushil1940@gmail.com>
2016-11-30 22:40:59 -0500

    Added hardcoded applications for now

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {StudentApplication} from "../models/student-application.model";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  applications: StudentApplication[] = [new StudentApplication(3000.1, 100, 300, 80), new StudentApplication(200, 0.5, 500, 1000), new StudentApplication(4165347384783.4865, 4523, 453, 846), new StudentApplication(4865, 53, 865, 4865.54)];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
 }
