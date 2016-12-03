b599917454c9746a0a70801f221daa41c89fabf6
Rushil Perera <pererarushil1940@gmail.com>
2016-12-03 11:06:06 -0500

    Added logout option in new menu

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from "@angular/core";
+import {UserService} from "../services/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import {Component} from "@angular/core";
 })
 export class AppComponent {
   title = 'Travel Grant Application System';
+
+  constructor(public userService: UserService, private _router: Router) {}
+
+  logout() {
+    this.userService.logout();
+    this._router.navigate(['/login']);
+  }
 }
