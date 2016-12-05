ca8974553d7f6494b4a15052dc0a2ead1b457625
Rushil Perera <pererarushil1940@gmail.com>
2016-12-04 19:34:57 -0500

    Rearranged import statements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { Ng2SelectModule } from "ng2-material-select/dist/src/ng2-select.module";
 
 import { CookieService } from "angular2-cookie/services/cookies.service";
 import { LoggedInGuard } from "./logged-in.guard";
 import { UserService } from "./services/user.service";
+import { ApplicationService } from "./services/application.service";
+import { AlertsService } from "./services/alerts.service";
 
 import { routing } from "./app.routing";
 
 import { AppComponent } from './components/app.component';
-import { MaterialModule } from '@angular/material';
 import { LoginComponent } from './components/login.component';
 import { HomeComponent } from './components/home.component';
 import { RegisterComponent } from './components/register.component';
 import { StudentApplicationComponent } from './components/student-application.component';
-import {ApplicationService} from "./services/application.service";
 import { NewStudentApplicationComponent } from './components/new-student-application.component';
-import {AlertsService} from "./services/alerts.service";
-import {Ng2SelectModule} from "ng2-material-select/dist/src/ng2-select.module";
 
 @NgModule({
   declarations: [
