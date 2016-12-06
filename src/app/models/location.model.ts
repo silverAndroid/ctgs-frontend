b24eefb5142b090120bc63bdf52c61831bba8ca4
Rushil Perera <pererarushil1940@gmail.com>
2016-12-06 17:34:45 -0500

    Added getters and setters

--- src/app/models/location.model.ts
+++ src/app/models/location.model.ts
@@ -3,4 +3,20 @@
  */
 export class LocationModel {
   constructor(private _placeID: string, private _name: string){}
+
+  get placeID(): string {
+    return this._placeID;
+  }
+
+  set placeID(value: string) {
+    this._placeID = value;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
 }
