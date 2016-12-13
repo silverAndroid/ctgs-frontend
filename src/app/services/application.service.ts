a3421cc6ef8887f6539448434746f397a46646e1
Rushil Perera <pererarushil1940@gmail.com>
2016-12-13 00:37:05 -0500

    Formatted code and added dates and location

--- src/app/services/application.service.ts
+++ src/app/services/application.service.ts
@@ -13,49 +13,53 @@ import {AlertsService} from "./alerts.service";
 @Injectable()
 export class ApplicationService {
 
-  private recommendationSource = new Subject<RecommendationModel>();
-  recommendation$ = this.recommendationSource.asObservable();
+    private recommendationSource = new Subject<RecommendationModel>();
+    recommendation$ = this.recommendationSource.asObservable();
 
-  constructor(private _http: Http, private _snackbar: AlertsService) {}
+    constructor(private _http: Http, private _snackbar: AlertsService) {
+    }
 
-  getApplications(role: string, username: string): Observable<JSONResponseModel> {
-    return this._http.get(`${role}/applications/${username}`)
-      .map(HTTPConnection.extractData)
-      .catch(err => {
-        return HTTPConnection.handleError(err, this._snackbar)
-      });
-  }
+    getApplications(role: string, username: string): Observable<JSONResponseModel> {
+        return this._http.get(`/${role}/applications/${username}`)
+            .map(HTTPConnection.extractData)
+            .catch(err => {
+                return HTTPConnection.handleError(err, this._snackbar)
+            });
+    }
 
-  createApplication(application: StudentApplication): Observable<JSONResponseModel> {
-    return this._http.post('/applications', {
-      registration: application.registrationCost,
-      transportation: application.transportationCost,
-      accommodation: application.accommodationCost,
-      meals: application.mealCost,
-      conferenceDetail: application.conferenceDescription,
-      presentationType: application.presentationOption,
-      presentationTitle: application.presentationTitle
-    })
-      .map(HTTPConnection.extractData)
-      .catch(err => {
-        return HTTPConnection.handleError(err, this._snackbar)
-      })
-  }
+    createApplication(application: StudentApplication): Observable<JSONResponseModel> {
+        return this._http.post('/applications', {
+            registration: application.registrationCost,
+            transportation: application.transportationCost,
+            accommodation: application.accommodationCost,
+            meals: application.mealCost,
+            conferenceDetail: application.conferenceDescription,
+            presentationType: application.presentationOption,
+            presentationTitle: application.presentationTitle,
+            startDate: application.conferenceStartDateTime,
+            endDate: application.conferenceEndDateTime,
+            location: application.conferenceLocation
+        })
+            .map(HTTPConnection.extractData)
+            .catch(err => {
+                return HTTPConnection.handleError(err, this._snackbar)
+            })
+    }
 
   makeRecommendation(recommendation: string, applicationID: number): Observable<TextResponseModel> {
-    return this._http.put('/application', {recommendation: recommendation, applicationId: applicationID})
+    return this._http.put('/application/recommend', {recommendation: recommendation, applicationId: applicationID})
       .map(HTTPConnection.extractData)
       .catch(err => {
         return HTTPConnection.handleError(err, this._snackbar)
       })
   }
 
-  emitRecommendation(recommendation: RecommendationModel) {
-    this.recommendationSource.next(recommendation);
-  }
+    emitRecommendation(recommendation: RecommendationModel) {
+        this.recommendationSource.next(recommendation);
+    }
 }
 
 export class RecommendationModel {
-  constructor(public applicationID: number, public wasAccepted: boolean) {
-  }
+    constructor(public applicationID: number, public wasAccepted: boolean) {
+    }
 }
