62f9876f134c0e834cf73366b707a7767f56172e
Rushil Perera <pererarushil1940@gmail.com>
2016-12-13 00:39:46 -0500

    New constant

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -5,5 +5,6 @@ export class Constants {
   static CONST_ACCEPTED = 'Accepted';
   static CONST_REJECTED = 'Rejected';
   static CONST_PENDING = 'Pending';
+  static CONST_MAKE_CHANGES = 'Make Changes';
   static CONST_ROLES = [{value: 'supervisor', label: 'Supervisor'}, {value: 'student', label: 'Requester'}, {value: 'admin', label: 'Admin'}];
 }
