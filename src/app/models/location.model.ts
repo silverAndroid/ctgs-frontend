bed5874c5f145fae8240dde183d8b2907f995780
Rushil Perera <pererarushil1940@gmail.com>
2016-12-13 00:35:56 -0500

    Made placeID optional and added optional parameter url

--- src/app/models/location.model.ts
+++ src/app/models/location.model.ts
@@ -2,21 +2,30 @@
  * Created by silver_android on 06/12/16.
  */
 export class LocationModel {
-  constructor(private _placeID: string, private _name: string){}
+    constructor(private _name: string, private _placeID?: string, private _url?: string) {
+    }
 
-  get placeID(): string {
-    return this._placeID;
-  }
+    get placeID(): string {
+        return this._placeID;
+    }
 
-  set placeID(value: string) {
-    this._placeID = value;
-  }
+    set placeID(value: string) {
+        this._placeID = value;
+    }
 
-  get name(): string {
-    return this._name;
-  }
+    get name(): string {
+        return this._name;
+    }
 
-  set name(value: string) {
-    this._name = value;
-  }
+    set name(value: string) {
+        this._name = value;
+    }
+
+    get url(): string {
+        return this._url;
+    }
+
+    set url(value: string) {
+        this._url = value;
+    }
 }
