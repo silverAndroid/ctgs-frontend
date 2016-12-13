943461308695374ecfc36ad72f4390aefec5816e
Rushil Perera <pererarushil1940@gmail.com>
2016-12-13 00:40:15 -0500

    New auto-generated tests

---
+++ src/app/tests/date-time-validator.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { DateTimeValidatorDirective } from '../directives/date-time-validator.directive';
+
+describe('DateTimeValidatorDirective', () => {
+  it('should create an instance', () => {
+    let directive = new DateTimeValidatorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/tests/money-validator.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { MoneyValidatorDirective } from '../directives/money-validator.directive';
+
+describe('MoneyValidatorDirective', () => {
+  it('should create an instance', () => {
+    let directive = new MoneyValidatorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
